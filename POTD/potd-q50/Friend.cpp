5579e78c9b693aa9459cb0d16fa01c3694d9ebff
Feng <qingf1995@gmail.com>
2020-04-13 16:29:03 -0500

    added potd 50

---
+++ POTD/potd-q50/Friend.cpp
@@ -0,0 +1,30 @@
+#include "Friend.h"
+
+int find(int x, std::vector<int>& parents) {
+    return (parents[x] == x) ? x : find(parents[x], parents);
+}
+
+int findCircleNum(std::vector<std::vector<int>>& M) {
+    if (M.empty()) {return 0;}
+
+    int n = M.size();
+    std::vector<int> parents;
+    for (int i = 0; i < n; i++) {
+        parents.push_back(i);
+    }
+    int res = n;
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (M[i][j] == 1) {
+                int parent_i = find(i, parents);
+                int parent_j = find(j, parents);
+                if (parent_i != parent_j) {
+                    parents[i] = parent_j;
+                    res--;
+                }
+            }
+        }
+    }
+
+    return res;
+}
